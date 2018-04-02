38ab3c0ea036127f2a58ece7a92ef0f05c8a3467
ДаукеновД <@223_11>
2018-04-02 22:07:52 +0600

    Add project files.

---
+++ Bankomat/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KKB.Bank.Module;//PODKLUCHILIS K PAPKE
+using GeneratorName;
+
+namespace Bankomat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Client> ListClient = new List<Client>();
+
+            Generator g = new Generator();
+
+            Client Cl = new Client();
+            Cl.DOB = DateTime.Now.AddYears(-60);
+            Cl.FullName = g.GenerateDefault(Gender.man);
+            Cl.IIN = "970131301448";
+            Cl.Login = "QWE";
+            Cl.Password = "123";
+            Cl.PhoneNum = "87778139374";
+
+            ListClient.Add(Cl);
+        }
+    }
+}
---
+++ KKB.Bank/Module/Client.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KKB.Bank.Module
+{
+    public class Client
+    {
+        public Client()
+        {
+            ListAccount = new List<Account>();
+        }
+        private string FullName_;
+        public string FullName
+        {
+            get
+            {
+                return FullName_;
+            }
+            set
+            {
+                FullName_ = value.Replace("?<center><b><font size =7>", "")
+                    .Replace("?<\font><\b><\center>", "")
+            }
+        }
+
+        public string IIN_;
+        public string IIN
+        {
+            get { return IIN_; }
+            set
+            { if (value.Length == 12)
+                {
+                    IIN_ = value;
+                }
+                else
+                {
+                    throw new Exception("nekkorektnyi IIN");
+                }
+            }
+        }
+        public DateTime DOB { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string PhoneNum { get; set; }
+        List<Account> ListAccount;
+
+        public void ClientInfoPrint(Client cl)
+        {
+            Console.WriteLine(
+                "{0}\n,{1}\n,{2}\n,{3}\n,{4}\n,{5}\n", 
+                FullName,IIN,Login,Password,
+                );
+        }
+    }
+}
